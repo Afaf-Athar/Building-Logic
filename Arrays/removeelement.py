aac967613b9afb829ae83e93b67998c2e58e1466
Afaf Athar <afaf.athar@powerschool.com>
2021-03-17 23:09:32 +0530

    Arrays_Leetcode

--- Arrays/removeelement.py
+++ Arrays/removeelement.py
@@ -1,17 +1,19 @@
-def removeelements(nums,val):
 
+#https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3247/
+
+def removeelements(nums,val):
     i = 0
     len_nums = len(nums)
     while(i < len_nums):
         if(nums[i] == val):
-            nums[i] = nums[i-1]
+            nums.remove(nums[i])
+            len_nums = len_nums-1
+            i = i-1
         i = i+1
-        return nums
-                
    
 
 
 val = 2
 nums = [1,2,2,3,1]
 removeelements(nums,val)
-# print(nums)
+print(nums)
